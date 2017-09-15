258fe2c78ad1d4777b4a2be0a55b87b7c102ede1
Vlad Kolesnikov <vladkol@microsoft.com>
2017-09-15 11:35:01 -0700

    Initial version

---
+++ HolographicMultiView/App.xaml.h
@@ -0,0 +1,28 @@
+﻿//
+// App.xaml.h
+// Declaration of the App class.
+//
+
+#pragma once
+
+#include "App.g.h"
+
+namespace HolographicXAMLView
+{
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	ref class App sealed
+	{
+	protected:
+		virtual void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs^ e) override;
+
+	internal:
+		App();
+		int holographicViewId = 0;
+
+	private:
+		void OnSuspending(Platform::Object^ sender, Windows::ApplicationModel::SuspendingEventArgs^ e);
+		void OnNavigationFailed(Platform::Object ^sender, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs ^e);
+	};
+}
---
+++ HolographicMultiView/NoHMDPage.xaml.h
@@ -0,0 +1,33 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "NoHMDPage.g.h"
+#include "Common/StepTimer.h"
+#include <vector>
+#include <ppltasks.h>
+#include <functional>
+#include <collection.h>
+
+namespace HolographicXAMLView
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class NoHMDPage sealed
+	{
+	public:
+		NoHMDPage();
+
+	private:
+		void Page_Loaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void OnIsAvailableChanged(Platform::Object ^sender, Platform::Object ^args);
+
+		Platform::Agile<Windows::UI::Core::CoreWindow> m_xamlViewWindow;
+		int m_xamlViewId = -1;
+		int m_holographicViewId = -1;
+	};
+}
